18976507c09171ccaddc1662f05d0e5f40ad2826
Ganesh6999 <Kumar.ganeshm39@gmail.com>
2024-09-28 12:00:58 +0530

    Add files via upload

---
+++ Casting.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+now=datetime.now()
+print(now)
+
+
+a=int("10")
+b=int("50")
+print(type(a+b))
+print('Casting int:', a+b)
+
+a=str(10)
+b=str(50)
+print(type(a+b))
+print('Casting str: ',a+b)
+
+
+
+
+
